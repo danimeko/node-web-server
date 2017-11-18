d0a8eda95415a476f8181f62c95c41bf14aa8a12
Daniel <daniel.mengistu@hotmail.com>
2017-11-18 22:32:59 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials( __dirname + '/views/partials');
+app.set('view engine' , 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req , res , next)=>{
+	let date = new Date().toString();
+	let log = date + req.url;	
+	fs.appendFile('Server.log' , log + '\n',(err)=>{
+		console.log('unable to write');
+	});
+	next();
+});
+
+app.use((req,res,next)=>{
+	res.render('mentinance.hbs');
+});
+hbs.registerHelper('getCurrentYear' , ()=>{
+	return new Date().getFullYear();
+})
+app.get('/' , (req , res)=>{
+	res.render('home.hbs' , {
+		pageTitle : 'Home page',
+		welcomeMessage : 'Welcome'
+	});
+});
+
+
+app.get('/bad' , (req , res)=>{
+	res.send({
+		message : 'Error' 
+	});
+});
+
+app.get('/about' ,(req , res )=>{
+	res.render('about.hbs', {
+		pageTitle : 'About page',
+	});
+})
+
+
+app.listen(3000 , ()=>{
+	console.log('Server is up and runing on port 3000');
+});
